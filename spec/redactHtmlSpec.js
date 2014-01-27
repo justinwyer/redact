0766f138478ba7c0c1b353789d4046ba651b1717
Carlos Lopes <carlosaml@gmail.com>
2014-01-27 13:49:38 +0200

    Option to keep or remove the feature description from redacted HTML

--- spec/redactHtmlSpec.js
+++ spec/redactHtmlSpec.js
@@ -7,18 +7,28 @@ describe("redact html", function () {
     expect(redact.redactHtml(html, {aToggle: false})).toEqual("<div></div>");
   });
 
-  it("should know when not to remove a feature from the html", function() {
-    var html = '<div><div feature="aToggle"></div></div>';
-    expect(redact.redactHtml(html, {aToggle: true})).toEqual('<div><div feature="aToggle"></div></div>');
+  it("should know when not to remove a feature from the html but keep the descriptor", function() {
+    var html = '<div><div feature="aToggle">toggled on content</div></div>';
+    expect(redact.redactHtml(html, {aToggle: true}, true)).toEqual('<div><div feature="aToggle">toggled on content</div></div>');
   });
 
-  it("should know how to remove an inner feature from the html", function() {
+  it("should know when not to remove a feature from the html but to remove the descriptor", function() {
+    var html = '<div><div feature="aToggle">toggled on content</div></div>';
+    expect(redact.redactHtml(html, {aToggle: true}, false)).toEqual('<div><div>toggled on content</div></div>');
+  });
+
+  it("should know how to remove an inner feature from the html but keep the descriptor", function() {
     var html = '<div><div feature="aToggle"><div><div feature="anotherToggle"></div></div></div></div>';
-    expect(redact.redactHtml(html, {aToggle: true, anotherToggle: false})).toEqual('<div><div feature="aToggle"><div></div></div></div>');
+    expect(redact.redactHtml(html, {aToggle: true, anotherToggle: false}, true)).toEqual('<div><div feature="aToggle"><div></div></div></div>');
+  });
+
+  it("should know how to remove an inner feature from the html but to remove the descriptor", function() {
+    var html = '<div><div feature="aToggle">toggled on content (outer)<div>more toggled on content<div feature="anotherToggle">toggled off content (inner)</div></div></div></div>';
+    expect(redact.redactHtml(html, {aToggle: true, anotherToggle: false}, false)).toEqual('<div><div>toggled on content (outer)<div>more toggled on content</div></div></div>');
   });
 
   it("should know how to remove an outer feature from the html", function() {
-    var html = '<div><div feature="aToggle"><div><div feature="anotherToggle"></div></div></div></div>';
-    expect(redact.redactHtml(html, {aToggle: false, anotherToggle: true})).toEqual("<div></div>");
+    var html = '<div>toggleless content<div feature="aToggle"><div><div feature="anotherToggle"></div></div></div></div>';
+    expect(redact.redactHtml(html, {aToggle: false, anotherToggle: true})).toEqual("<div>toggleless content</div>");
   });
 });
