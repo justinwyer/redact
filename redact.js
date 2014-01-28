997a443865dab5137f1104766f4c8132b293c5b6
Carlos Lopes <carlosaml@gmail.com>
2014-01-28 10:02:51 +0200

    Reformatting redact.js file

--- redact.js
+++ redact.js
@@ -1,4 +1,4 @@
-var acorn = require("acorn");
+var acorn = require('acorn');
 var _ = require('lodash');
 var cheerio = require('cheerio');
 
@@ -6,7 +6,7 @@ function collectIfStatements(data, toggles) {
   function recurse(acc, value, key, collection) {
     if (isIfStatement(value) && isFeatureToggle(value, toggles)) {
       acc.push({name: value.test.property.name, toggled: toggles[value.test.property.name],
-            start: value.consequent.start, end: value.consequent.end });
+        start: value.consequent.start, end: value.consequent.end });
       return _.foldl([value.consequent], recurse, acc);
     } else if (_.has(value, "body")) {
       return _.foldl(value.body, recurse, acc);
@@ -14,6 +14,7 @@ function collectIfStatements(data, toggles) {
       return acc;
     }
   }
+
   return _.foldl([acorn.parse(data)], recurse, []);
 }
 
@@ -23,33 +24,35 @@ function isIfStatement(node) {
 
 function isFeatureToggle(ifStatement, toggles) {
   return _.has(ifStatement, "test") &&
-         _.has(ifStatement.test, "property") &&
-         _.has(ifStatement.test, "object") &&
-         ifStatement.test.object.name === 'feature' &&
-         _.contains(_.keys(toggles), ifStatement.test.property.name);
+    _.has(ifStatement.test, "property") &&
+    _.has(ifStatement.test, "object") &&
+    ifStatement.test.object.name === 'feature' &&
+    _.contains(_.keys(toggles), ifStatement.test.property.name);
 }
 
 function redactJavascript(code, features) {
   var nextByte = 0;
+
   function recurse(redactedCode, value, index, features) {
     if (!value.toggled && value.start >= nextByte) {
       redactedCode += code.substring(nextByte, value.start) +
-                      "{} // " + value.name + " redacted";
-        nextByte = value.end;
+        "{} // " + value.name + " redacted";
+      nextByte = value.end;
     }
     return redactedCode;
   }
+
   return _.foldl(collectIfStatements(code, features), recurse, "") + code.substring(nextByte, code.length);
 }
 
 function redactHtml(html, features, keepDescriptor) {
-    keepDescriptor = keepDescriptor || false;
-    var $ = cheerio.load(html);
-    _.forEach(features, function(toggled, feature, collection) {
-      if (!toggled) $('[feature="' + feature + '"]').remove();
-      if (!keepDescriptor) $('[feature="' + feature + '"]').attr('feature', null);
-    });
-    return $.html();
+  keepDescriptor = keepDescriptor || false;
+  var $ = cheerio.load(html);
+  _.forEach(features, function (toggled, feature, collection) {
+    if (!toggled) $('[feature="' + feature + '"]').remove();
+    if (!keepDescriptor) $('[feature="' + feature + '"]').attr('feature', null);
+  });
+  return $.html();
 }
 
 exports.collectIfStatements = collectIfStatements;
