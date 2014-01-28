cd0cf3fdc3dfef5a91db1de86ca0d6ce67a0a959
Carlos Lopes <carlosaml@gmail.com>
2014-01-28 19:38:10 +0200

    Added tests to test negative scenarios, e.g. !feature.facebookLogin

--- spec/redactJavascriptSpec.js
+++ spec/redactJavascriptSpec.js
@@ -2,33 +2,49 @@ var acorn = require("acorn");
 var redact = require("../redact");
 
 describe("redact javascript", function () {
-  it("should know when to remove a feature from the code", function() {
-    var code = "if (feature.aToggle) console.log('its true');\nconsole.log('more stuff');";
-    expect(redact.redactJavascript(code, {aToggle: false})).toEqual(
-      "if (feature.aToggle) {} // aToggle redacted\nconsole.log('more stuff');");
+  describe("when checking if a feature is on", function () {
+    it("should know when to remove a feature from the code", function () {
+      var code = "if (feature.aToggle) console.log('its true');\nconsole.log('more stuff');";
+      expect(redact.redactJavascript(code, {aToggle: false})).toEqual(
+        "if (feature.aToggle) {} // aToggle redacted\nconsole.log('more stuff');");
+    });
+
+    it("should know when to NOT redact code if it is not explicitly a feature toggle", function () {
+      var code = "if (abc.propertyThatIsNotAToggleButHasTheSameNameAsAToggle) console.log('this is not a feature toggle');\nconsole.log('more stuff');";
+      expect(redact.redactJavascript(code, {propertyThatIsNotAToggleButHasTheSameNameAsAToggle: false})).toEqual(
+        "if (abc.propertyThatIsNotAToggleButHasTheSameNameAsAToggle) console.log('this is not a feature toggle');\nconsole.log('more stuff');");
+    });
+
+    it("should know when not to remove a feature from the code", function () {
+      var code = "if (feature.aToggle) console.log('its true');";
+      expect(redact.redactJavascript(code, {aToggle: true})).toEqual(
+        "if (feature.aToggle) console.log('its true');");
+    });
+
+    it("should know how to remove an inner feature from the code", function () {
+      var code = "if (feature.aToggle)\n{ if (feature.anotherToggle) console.log('its true')\n};";
+      expect(redact.redactJavascript(code, {aToggle: true, anotherToggle: false})).toEqual(
+        "if (feature.aToggle)\n{ if (feature.anotherToggle) {} // anotherToggle redacted\n};");
+    });
+
+    it("should know how to remove an outer feature from the code", function () {
+      var code = "if (feature.aToggle) if (feature.anotherToggle) console.log('its true');";
+      expect(redact.redactJavascript(code, {aToggle: false, anotherToggle: true})).toEqual(
+        "if (feature.aToggle) {} // aToggle redacted");
+    });
   });
 
-  it("should know when to NOT redact code if it is not explicitly a feature toggle", function() {
-    var code = "if (abc.propertyThatIsNotAToggleButHasTheSameNameAsAToggle) console.log('this is not a feature toggle');\nconsole.log('more stuff');";
-    expect(redact.redactJavascript(code, {propertyThatIsNotAToggleButHasTheSameNameAsAToggle: false})).toEqual(
-      "if (abc.propertyThatIsNotAToggleButHasTheSameNameAsAToggle) console.log('this is not a feature toggle');\nconsole.log('more stuff');");
-  });
+  describe("when checking if a feature is off", function () {
+    it("should know when to keep a feature in the code", function () {
+      var code = "if (!feature.aToggle) console.log('aToggle is still off, come back later');\nconsole.log('more stuff');";
+      expect(redact.redactJavascript(code, {aToggle: false})).toEqual(
+        "if (!feature.aToggle) console.log('aToggle is still off, come back later');\nconsole.log('more stuff');");
+    });
 
-  it("should know when not to remove a feature from the code", function() {
-    var code = "if (feature.aToggle) console.log('its true');";
-    expect(redact.redactJavascript(code, {aToggle: true})).toEqual(
-      "if (feature.aToggle) console.log('its true');");
-  });
-
-  it("should know how to remove an inner feature from the code", function() {
-    var code = "if (feature.aToggle)\n{ if (feature.anotherToggle) console.log('its true')\n};";
-    expect(redact.redactJavascript(code, {aToggle: true, anotherToggle: false})).toEqual(
-      "if (feature.aToggle)\n{ if (feature.anotherToggle) {} // anotherToggle redacted\n};");
-  });
-
-  it("should know how to remove an outer feature from the code", function() {
-    var code = "if (feature.aToggle) if (feature.anotherToggle) console.log('its true');";
-    expect(redact.redactJavascript(code, {aToggle: false, anotherToggle: true})).toEqual(
-      "if (feature.aToggle) {} // aToggle redacted");
+    xit("should know when to remove a feature from the code", function () {
+      var code = "if (!feature.aToggle) console.log('aToggle is still off, so I should not be here');\nconsole.log('more stuff');";
+      expect(redact.redactJavascript(code, {aToggle: true})).toEqual(
+        "if (!feature.aToggle) {} \nconsole.log('more stuff');");
+    });
   });
 });
